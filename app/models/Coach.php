4cd912e8a61625aa81d604069f19835cc9f854ce
Yamila Holik <shami.nair.holik@gmail.com>
2026-05-22 20:00:05 -0300

    fix: corrige redirección 404 al intentar ir a login desde registro con cuenta existente

--- app/models/Coach.php
+++ app/models/Coach.php
@@ -9,7 +9,7 @@ class Coach {
     }
 
 
-public function getAll(){
+public function getAllCoaches(){
     $sql = "SELECT c.* , u.email
     FROM coaches c
     INNER JOIN users u ON c.user_id = c.id
@@ -19,7 +19,7 @@ public function getAll(){
     return $stmt->fetchAll(PDO::FETCH_ASSOC);
 }
 
-public function create(array $data){
+public function createCoach(array $data){
 
 $sql = "INSERT INTO coaches (user_id, first_name, last_name, phone, specialty)
 VALUES (?, ?, ?, ?, ?)";
@@ -32,25 +32,36 @@ $data['last_name'],
 $data['phone'],
 $data['specialty']
 ]);
+}
 
 
-public function getUserById(int $user_id){
+public function getCoachById(int $user_id){
 
 $sql = "SELECT c.*, u.email 
              FROM coaches c 
              INNER JOIN users u ON c.user_id = u.id 
              WHERE c.user_id = ? AND c.deleted_at IS NULL 
              LIMIT 1"
+;
 
 $stmt = $this->db->prepare($sql);
 $stmt->execute([$user_id]);
 return $stmt->fetch(PDO::FETCH_ASSOC);
 }
+
+
+public function updateCoach(int $user_id, array $data){
+    $sql = "UPDATE coaches SET first_name = ?, last_name = ?, phone = ?, specialty = ? WHERE user_id = ? AND deleted_at IS NULL";
+   
+    $stmt = $this->db->prepare($sql);
+    
+    return $stmt->execute([
+        $data['first_name'],
+        $data['last_name'],
+        $data['phone'],
+        $data['specialty'],
+        $user_id
+    ]);
 }
-
-
-
-
-
 
 }
